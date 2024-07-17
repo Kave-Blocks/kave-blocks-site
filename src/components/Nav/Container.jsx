1e08c92657e7b7b46a0f445bf26dd487215b23d8
jasperayawan <ejayawan22@gmail.com>
2024-07-17 18:23:41 +0800

    update frontend

--- src/components/Nav/Container.jsx
+++ src/components/Nav/Container.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import HeaderLogo from "./HeaderLogo";
 import Location from "./Location";
+import DivFadeLeft from "../../animation/DivFadeLeft";
 
 const Container = () => {
   return (
     <div className="mx-auto max-w-screen-desktop w-full py-5 font-Raleway px-4 md:px-20">
       <div className="w-full flex justify-between items-center">
         <HeaderLogo />
-       <a href="https://calendly.com/kaveguild" target="_blank" className="bg-[#C86B1F] text-white px-5 py-2 rounded-full font-Raleway font-semibold">
+        <DivFadeLeft>
+        <a href="https://calendly.com/kaveguild" target="_blank" className="bg-[#C86B1F] text-white px-5 py-2 rounded-full font-Raleway font-semibold">
         Git in Touch
        </a>
+        </DivFadeLeft>
       </div>
     </div>
   );
